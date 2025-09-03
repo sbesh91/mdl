b130c1e34178396ca9c82e770246d09b1ca6d43d
Steven Beshensky <s.beshensky@gmail.com>
2025-09-03 08:31:21 -0500

    remove wrangler config while it is not working

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [fresh(), tailwindcss(), cloudflare()],
+  plugins: [fresh(), tailwindcss()],
 });
