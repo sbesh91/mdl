7dc5dcde64c0ff4608fcaf62777f31e9be519f8f
Steven Beshensky <s.beshensky@gmail.com>
2025-11-08 10:53:37 -0600

    Add web app manifest and meta tags

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,12 @@ export default define.page(function App({ Component }) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#2b2b2d" />
+        <meta name="description" content="MDL Web Application" />
+
         <link rel="icon" href="/favicon.svg" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/logo.svg" />
 
         <title>mdl</title>
       </head>
