76db872e0afe6ed39dbde5aa6a5bcd0aaad213ee
Steven Beshensky <s.beshensky@gmail.com>
2025-11-09 11:20:58 -0600

    Add Wrangler deployment for Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [fresh(), tailwindcss()],
+  publicDir: "static",
+  plugins: [fresh(), tailwindcss(), cloudflare()],
 });
